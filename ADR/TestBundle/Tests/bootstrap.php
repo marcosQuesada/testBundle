9f3aaf637b5846fcb5754180402ea56fd1301b0e
marcos <marcos@marcos-N73SM.(none)>
2012-11-16 00:25:45 +0100

    recovered original bundle autoloader

--- ADR/TestBundle/Tests/bootstrap.php
+++ ADR/TestBundle/Tests/bootstrap.php
@@ -1,11 +1,8 @@
 <?php
-$file = __DIR__.'/../../../../../../app/bootstrap.php.cache';
-$autoload = require_once $file;
 
-/*
 $file = __DIR__.'/../../../../../autoload.php';
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies to run test suite (composer install --dev).');
 }
 
-$autoload = require_once $file;*/
+$autoload = require_once $file;
